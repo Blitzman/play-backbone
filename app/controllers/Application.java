c864ab7089bb487b12bfd5147c9e12dbf37ea4a3
Alberto García <albertgg93@gmail.com>
2014-05-04 17:39:46 +0200

    Login screen, form and session.
    A login screen view has been implemented so that the route /login
    redirects to it. A simple form accepting an email and a password
    has been added as well on the login.scala.html view. In addition,
    that form has been validated and a simple authentication procedure
    is followed to log in with an existing user preloaded with our
    YAML initial data.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,14 +2,52 @@ package controllers;
 
 import models.Project;
 import models.Task;
+import models.User;
 import play.*;
+import play.data.*;
 import play.mvc.*;
 import views.html.*;
 
-public class Application extends Controller {
+import static play.data.Form.*;
 
-    public static Result index() {
+public class Application extends Controller 
+{
+	public static class Login
+	{
+		public String email;
+		public String pass;
+
+		public String validate()
+		{
+			if (User.authenticate(email, pass) == null)
+				return "Invalid user or password.";
+		
+			return null;
+		}
+	}
+	
+	public static Result authenticate()
+	{
+		Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+
+		if (loginForm.hasErrors())
+			return badRequest(login.render(loginForm));
+		else
+		{
+			session().clear();
+			session("email", loginForm.get().email);
+
+			return redirect(routes.Application.index());
+		}
+	}
+	
+	public static Result login()
+	{
+		return ok(login.render(Form.form(Login.class)));
+	}
+	
+    public static Result index() 
+    {
         return ok(index.render(Project.find.all(), Task.find.all()));
     }
-
 }
